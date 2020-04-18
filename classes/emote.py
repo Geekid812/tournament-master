28b2469a53b33ad82ecbdd04e4f4a35de7a28152
Geekid812 <geekid812@gmail.com>
2020-04-18 19:02:21 +0200

    Rename emojis

--- classes/emote.py
+++ classes/emote.py
@@ -1,7 +1,7 @@
 # Emote Object
 
 class Emote:
-    silver_coin = "<:silver_coin:558719636020002827>"
-    logo = "<:wwoth_round:590195120540876800>"
+    silver_coin = "<:silverCoin:558719636020002827>"
+    logo = "<:wwothRound:590195120540876800>"
     check = "<:check:658367941288525825>"
     deny = "<:deny:658374396213329932>"
