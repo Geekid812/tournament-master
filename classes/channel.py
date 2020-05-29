a5b85a298597c29e68c196aca39fcd6f2c24f16e
Geekid812 <geekid812@gmail.com>
2020-05-29 16:24:14 +0200

    Add load retry for roles and channels

--- classes/channel.py
+++ classes/channel.py
@@ -1,17 +1,29 @@
 # Channel Object
 
+from time import sleep
+
 
 class Channel:
     def __init__(self, client):
-        # tchannel Temp Edit | Real ID: 553645306957398026
-        self.t_channel = client.get_channel(711652464885104701)
-        # tchat Temp Edit | Real ID: 553646158786854913
-        self.t_chat = client.get_channel(711652552114044948)
-        # tplanned Temp Edit | Real ID: 553645189168758795
-        self.t_planned = client.get_channel(575627727013412891)
-        self.logs = client.get_channel(553887905378992129)
-        self.t_logs = client.get_channel(553647392457621504)
-        # Real ID: 553886807373381635
-        self.bot_cmds = client.get_channel(711653087168823386)
-        self.t_demands = client.get_channel(561513445170741259)
-        self.error_logs = client.get_channel(707899679257657405)
+        for i in range(5):
+            # tchannel Temp Edit | Real ID: 553645306957398026
+            self.t_channel = client.get_channel(711652464885104701)
+            # tchat Temp Edit | Real ID: 553646158786854913
+            self.t_chat = client.get_channel(711652552114044948)
+            # tplanned Temp Edit | Real ID: 553645189168758795
+            self.t_planned = client.get_channel(575627727013412891)
+            self.logs = client.get_channel(553887905378992129)
+            self.t_logs = client.get_channel(553647392457621504)
+            # Real ID: 553886807373381635
+            self.bot_cmds = client.get_channel(711653087168823386)
+            self.t_demands = client.get_channel(561513445170741259)
+            self.error_logs = client.get_channel(707899679257657405)
+
+            all_channels = [getattr(self, c) for c in dir(self) if not c.startswith("_")]
+
+            if None not in all_channels: break
+            if i < 4:
+                print(f"Failed to load channels, retrying in 5 seconds. ({str(4 - i)} attempts left)")
+                sleep(5)
+            else:
+                print("Failed to load channels!")
--- classes/role.py
+++ classes/role.py
@@ -3,17 +3,28 @@
 # Importing libraries
 import discord.utils
 import core
+from time import sleep
 
 guildid = core.ReadJSON("config.json")["guild_id"]
 
 class Role:
     def __init__(self, client):
-        guild = client.get_guild(guildid)
-        self.t_organizer = discord.utils.get(guild.roles, id=554385062313852968)
-        self.schedule = discord.utils.get(guild.roles, id=555429481972498472)
-        self.tournament = discord.utils.get(guild.roles, id=554309332418691083)
-        self.participant = discord.utils.get(guild.roles, id=554308520481390643)
-        self.spectator = discord.utils.get(guild.roles, id=692735821832519801)
-        self.temp_host = discord.utils.get(guild.roles, id=554385586954305558)
-        self.t_banned = discord.utils.get(guild.roles, id=553894764504678400)
-        self.t_host_blacklist = discord.utils.get(guild.roles, id=559395242063953932)
+        for i in range(5):
+            guild = client.get_guild(guildid)
+            self.t_organizer = discord.utils.get(guild.roles, id=554385062313852968)
+            self.schedule = discord.utils.get(guild.roles, id=555429481972498472)
+            self.tournament = discord.utils.get(guild.roles, id=554309332418691083)
+            self.participant = discord.utils.get(guild.roles, id=554308520481390643)
+            self.spectator = discord.utils.get(guild.roles, id=692735821832519801)
+            self.temp_host = discord.utils.get(guild.roles, id=554385586954305558)
+            self.t_banned = discord.utils.get(guild.roles, id=553894764504678400)
+            self.t_host_blacklist = discord.utils.get(guild.roles, id=559395242063953932)
+
+            all_roles = [getattr(self, r) for r in dir(self) if not r.startswith("_")]
+
+            if None not in all_roles: break
+            if i < 4:
+                print(f"Failed to load roles, retrying in 5 seconds. ({str(4 - i)} attempts left)")
+                sleep(5)
+            else:
+                print("Failed to load roles!")
